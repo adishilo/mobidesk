b0fde71a4059f33a224231bdc321db0b38b65455
Adi Shilo <adi@testim.io>
2020-04-25 17:26:16 +0300

    WIP: Add expo-task-manager

--- client-app/App.tsx
+++ client-app/App.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+const LOCATION_TASK_NAME = 'background-location-task';
+
+export default class Component extends React.Component {
+    onPress = async () => {
+        const { status } = await Location.requestPermissionsAsync();
+        if (status === 'granted') {
+            await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+                accuracy: Location.Accuracy.Balanced,
+            });
+        }
+    };
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this.onPress}>
+                <Text>Enable background location</Text>
+            </TouchableOpacity>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+TaskManager.defineTask(LOCATION_TASK_NAME, (param: any) => {
+    const { data, error } = param;
+
+    if (error) {
+        // Error occurred - check `error.message` for more details.
+        console.log('Error in task', error);
+        return;
+    }
+    if (data) {
+        const { locations } = data;
+
+        console.log(locations);
+        // do something with the locations captured in the background
+    }
 });
